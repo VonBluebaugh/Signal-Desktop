2b2d35aceedb27a0e8be0c1d4b9c83a146df89c9
Daniel Gasienica <daniel@signal.org>
2018-03-16 17:32:17 -0400

    Move `writeAttachmentData` into `Attachments`
    Encapsulates all attachment operations that require Node.js / Electron APIs,
    e.g. file system access.

--- preload.js
+++ preload.js
@@ -110,15 +110,18 @@
   window.autoOrientImage = autoOrientImage;
 
   // ES2015+ modules
+  const attachmentsPath = Attachments.getPath(app.getPath('userData'));
+  const readAttachmentData = Attachments.readData(attachmentsPath);
+  const writeAttachmentData = Attachments.writeData(attachmentsPath);
+
   window.Signal = window.Signal || {};
   window.Signal.Logs = require('./js/modules/logs');
   window.Signal.OS = require('./js/modules/os');
   window.Signal.Migrations = window.Signal.Migrations || {};
-  const attachmentsPath = Attachments.getPath(app.getPath('userData'));
-  const { writeAttachmentData } = require('./app/types/attachment/write_attachment_data');
   // Injected context functions to keep `Message` agnostic from Electron:
   window.Signal.Migrations.context = {
-    writeAttachmentData: writeAttachmentData(attachmentsPath),
+    readAttachmentData,
+    writeAttachmentData,
   };
   window.Signal.Migrations.V17 = require('./js/modules/migrations/17');
   window.Signal.Types = window.Signal.Types || {};
